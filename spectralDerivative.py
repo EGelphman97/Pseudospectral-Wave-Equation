e9ed84490ddef7a4658ffef3c109d0c787ec8c03
Eric Gelphman <egelphman17@gmail.com>
2023-10-23 17:35:31 -0600

    Add files via upload

--- spectralDerivative.py
+++ spectralDerivative.py
@@ -5,7 +5,7 @@ import sys
 
 PI = np.pi
 
-def spectralLaplacian(y, k1, k2, c2):
+def spectralLaplacian(y, ik1, ik2, c2):
     """
     Function to calculate the Laplacian u = Del*((c^2(x1,x2))Del(y(x1,x2))) in the Fourier domain
     where Del is the two-dimensional gradient operator using Algorithm 7 from the paper "Notes on 
@@ -14,8 +14,8 @@ def spectralLaplacian(y, k1, k2, c2):
 
     Parameters:
         y: y(x1,x2)
-        k1: Meshgrid of wave numbers for dimension 1
-        k2: Meshgrid of wave numbers for dimension 2 
+        ik1: i*Meshgrid of wave numbers for dimension 1 
+        ik2: i*Meshgrid of wave numbers for dimension 2 
         c2: The function representing wave speed c^2(x1,x2)
 
     Return: 
@@ -23,31 +23,29 @@ def spectralLaplacian(y, k1, k2, c2):
     """
     #Step 1
     Yk = sfft.fft2(y)
-    Gk1 = np.multiply(1j*k1, Yk)
-    Gk2 = np.multiply(1j*k2, Yk)
-    gn1 = np.real(sfft.ifft(Gk1))
-    gn2 = np.real(sfft.ifft(Gk2))
+    gn1 = np.real(sfft.ifft2(ik1*Yk))
+    gn2 = np.real(sfft.ifft2(ik2*Yk))
     #Step 2
     vn1 = np.multiply(c2, gn1)
     vn2 = np.multiply(c2, gn2)
     #Step 3
     Vk1 = sfft.fft2(vn1)
     Vk2 = sfft.fft2(vn2)
-    Uk = np.multiply(1j*k1, Vk1) + np.multiply(1j*k2, Vk2)
-    return np.real(sfft.ifft2(Uk))
+    Uk = (ik1*Vk1) + (ik2*Vk2)
+    return sfft.ifft2(Uk)
 
 def main():
     L = 2.0*PI
     N1 = N2 = 256 #Assume N1 = N2 in 2D FFT
     k_neg = np.arange(-N1//2 + 1,0,1)
-    k_pos = np.concatenate((np.arange(N1//2), np.array([0])))*(N1/(2*PI))
-    kk1 = kk2 = np.concatenate((k_pos, k_neg))
-    k1, k2 = np.meshgrid(kk1, kk2, indexing='xy')
+    k_pos = np.concatenate((np.arange(N1//2), np.array([0])))
+    kk = np.concatenate((k_pos, k_neg))
+    ikk = 1j*kk
+    k1, k2 = np.meshgrid(ikk, ikk, indexing='ij')
 
     #True spectral derivative
     xc = np.linspace(0, 2*PI, num=N1)
-    yc = np.linspace(0, 2*PI, num=N1)
-    x, y = np.meshgrid(xc, yc, indexing='xy')
+    x, y = np.meshgrid(xc, xc, indexing='ij')
     Exy = np.exp(-10.0*(np.power(x - PI, 2) + np.power(y - PI, 2)))
     dEdx = np.multiply(-20.0*(x - PI), Exy)
     dEdy = np.multiply(-20.0*(y - PI), Exy)
